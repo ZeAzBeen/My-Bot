33249873428dbf8fd4d43904c122e83992b5aa14
Kykygamer02 <57462049+Kykygamer02@users.noreply.github.com>
2019-11-06 21:38:17 +0100

    Create Packtage.js

---
+++ Packtage.js
@@ -0,0 +1,16 @@
+
+{
+			"name": "bot",
+			"version": "1.0.0",
+			"description": "",
+			"main": "main.js"
+			"script": {
+				 "start": "node main.js" 
+			},
+			"keywords": [], 
+			"autor": "",
+			"licence": ""
+			"dependencies": {
+					"discord.js": "^11.3.2"
+					} 
+} 
